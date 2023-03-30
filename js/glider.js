b32c4adb98f5338924ae27bcc7efacd3abfd89c0
viente.matheus <matheus31102001@gmail.com>
2023-03-30 15:42:17 -0300

    primeiro commit projeito r4

---
+++ js/glider.js
@@ -0,0 +1,16 @@
+
+
+  new Glider(document.querySelector('.glider'), {
+	slidesToShow: 3,
+	slidesToScroll: 2,
+	draggable: true,
+	dots: '.dots',
+	arrows: {
+	  prev: '.glider-prev',
+	  next: '.glider-next'
+	}
+  });
+
+
+
+
